70d3a13118d06e4ec124ce330b15fe27a6ea3b8c
SD6399 <david-shapiro@mail.ru>
2024-09-24 15:57:29 +0300

    Embedding QR in center in spectrum

--- scpetrcal_halftone.py
+++ scpetrcal_halftone.py
@@ -4,8 +4,8 @@ from skimage import io
 from scipy.fftpack import fft2, fftshift
 from skimage import data, color
 
-def spectr_to_spatial(qr,size_WM):
 
+def spectr_to_spatial(qr, size_WM):
     qr_back = np.zeros((size_WM, size_WM)).astype(complex)
 
     qr = qr.astype(complex)
@@ -16,28 +16,30 @@ def spectr_to_spatial(qr,size_WM):
                 comp_val = np.e ** (complex(0, 1) * np.random.uniform(0, 2 * np.pi))
                 qr[i][j] = comp_val
 
-    qr_back[1:66, 1:33] = qr[:, :32]
+    qr_back[100 + 1:100 + 66, 100 + 1:100 + 33] = qr[:, :32]
     for i in range(65):
         for j in range(32):
-            qr_back[-(i + 1), -(j + 1)] = np.conj(qr_back[i + 1, j + 1])
+            # print(qr_back[i + 1, j + 1], (100+i + 1, 100+ j + 1), (-100- (i + 1),- 100- (j + 1)),
+            #       (256-(100 + i + 1),256-(100 + j + 1)))
+            qr_back[-100 - (i + 1), -100 - (j + 1)] = np.conj(qr_back[100 + i + 1, 100 + j + 1])
 
-    qr_back[1:66, -33:] = qr[:, 32:]
-    print(qr_back[1:66, -33:].shape, qr[:, 32:].shape)
+    qr_back[100+1:100+66, -100-33:-100] = qr[:, 32:]
+    # print(qr_back[1:66, -33:].shape, qr[:, 32:].shape)
     for i in range(66):
         for j in range(1, 34):
-            qr_back[-(i + 1), j] = np.conj(qr_back[i + 1, -j])
+            qr_back[-100-(i + 1),100+ j] = np.conj(qr_back[100+i + 1, -100-j])
 
     my_fft = np.fft.fft2(qr_back)
-    print( np.min(my_fft), np.max(my_fft))
+    print(np.min(my_fft), np.max(my_fft))
     my_fft = 255 * (my_fft - np.min(my_fft)) / (np.max(my_fft) - np.min(my_fft))
-    return my_fft,
+    print(my_fft.imag)
+    return my_fft.real
 
 
 def check_spatial2spectr(qr_spatial):
-
     ifft = np.fft.ifft2(qr_spatial)
 
-    ifft[0, 0] = 0 # - КОСТЫЛЬ
+    ifft[0, 0] = 0  # - КОСТЫЛЬ
 
     ifft_r = np.abs(ifft)
     my_ifft = 255 * (ifft_r - np.min(ifft_r)) / (np.max(ifft_r) - np.min(ifft_r))
@@ -56,14 +58,14 @@ def energy_spector(image):
     norm_eval = 255 * (ifft - np.min(ifft)) / (np.max(ifft) - np.min(ifft))
     return norm_eval
 
-# qr_spectr = io.imread("data/spectral_qr_65_65.png")
-# spatial_qr = spectr_to_spatial(qr_spectr,512)
+
+qr_spectr = io.imread("data/spectral_qr_65_65.png")
+spatial_qr = spectr_to_spatial(qr_spectr, 512)
 # img1 = Image.fromarray(spatial_qr.astype('uint8'))
-# img1.save(r"data/spatial_spectr_wm_65.png")
+# img1.save(r"data/spatial_spectr_in_center_wm_65.png")
 
 # qr_spatal = io.imread("data/spatial_spectr_wm_65.png")
 
 # check_qr = check_spatial2spectr(qr_spatal)
 # img2 = Image.fromarray(check_qr.astype('uint8'))
 # img2.save(r"data/check_ifft_wm.png")
-
